08d5ec5059c8a47d8c25e2a3f25ee0f33d5f8687
unknown <j0h@example.com>
2020-07-09 11:31:35 +0300

    Includind another view in Blade (_form.blade.php and coped repead code), using new operator ?? null

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,24 +3,7 @@
 @section ('content')
     <form method="POST" action="{{ route ('posts.store') }}">
         @csrf
-        <p>
-            <label> Title</label>
-            <input type="text" name="title" value = "{{ old('title') }}"/> {{-- keep title value in form old helper--}}
-        </p>
-        <p>
-            <label> Content</label>
-            <input type="text" name="content" value = "{{ old('content') }}"/>
-        </p>
-
-        @if($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li> {{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        @include('posts._form')
 
         <button type="submit">Create!</button> 
 
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,26 +5,8 @@
           action="{{ route ('posts.update',['post' => $post->id]) }}"> {{-- specify parameter $post inside array and we should access to id which hae ben passed  --}}
         @csrf
         @method('PUT')
-        <p>
-            <label> Title</label>
-            <input type="text" name="title" 
-                value = "{{ old('title', $post->title) }}"/> {{-- $post->title keep atual post title--}}
-        </p>
-        <p>
-            <label> Content</label>
-            <input type="text" name="content" 
-                value = "{{ old('content', $post->content) }}"/>
-        </p>
-
-        @if($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li> {{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        
+        @include('posts._form')
 
         <button type="submit">Update!</button> 
 

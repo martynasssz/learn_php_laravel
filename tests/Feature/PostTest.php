0a074c93df5299ec2708ce77064d277484a056a1
unknown <j0h@example.com>
2020-07-21 10:45:06 +0300

    Testing for failure, it means when form fields are emty, we should see session flash mewssages

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -38,18 +38,36 @@ class PostTest extends TestCase
     }
 
     public function testStoreValid()
-        {
-            $params = [
-                'title' => 'Valid title',
-                'content' => 'At least 10 characters'
-            ]; 
-            
-            $this ->post('/posts', $params)
-                ->assertStatus(302) //302 status, then redirection successful
-                ->assertSessionHas('status'); //check if status variable inside in session
+    {
+        $params = [
+            'title' => 'Valid title',
+            'content' => 'At least 10 characters'
+        ]; 
+        
+        $this ->post('/posts', $params)
+            ->assertStatus(302) //302 status, then redirection successful
+            ->assertSessionHas('status'); //check if status variable inside in session
 
-            $this->assertEquals(session('status'), 'Blog post was created');  //reading session value using session function
-                
-        }
-    
+        $this->assertEquals(session('status'), 'Blog post was created!');  //reading session value using session function
+            
+    }
+
+    public function testStoreFail()
+    {
+        $params = [
+            'title' => 'x',
+            'content' => 'x'
+        ];
+
+        $this->post('/posts', $params)
+            ->assertStatus(302)
+            ->assertSessionHas('errors');
+
+        $messages = session('errors')->getMessages();
+
+        $this->assertEquals($messages['title'][0], 'The title must be at least 5 characters.');
+        $this->assertEquals($messages['content'][0], 'The content must be at least 10 characters.');
+
+        //dd($messages->getMessages()); //dd to see that message contains   
+    }
 }

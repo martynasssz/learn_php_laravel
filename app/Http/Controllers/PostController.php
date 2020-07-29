e6936b32e58fe985fc40a4d9a245b1fe39ba41d5
unknown <j0h@example.com>
2020-07-29 08:43:03 +0300

    Application:Implementing commnet lish. Add commnents in BlogPost show controller and show blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,7 +35,8 @@ class PostController extends Controller
      */    
     public function show($id) 
     {        
-        return view('posts.show', ['post' => BlogPost::findOrFail($id)]); 
+        return view('posts.show', [
+            'post' => BlogPost::with('comments')->findOrFail($id)]); 
     }
 
     public function create()

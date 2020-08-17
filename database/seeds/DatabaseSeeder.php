8a0747117fb7f08bb1fe0d2959b1fdc42cb0e396
unknown <j0h@example.com>
2020-08-17 16:47:37 +0300

    Model relations inside seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
@@ -22,8 +21,19 @@ class DatabaseSeeder extends Seeder
         //         'remember_token' => Str::random(10),
         // ]);
 
-        factory(App\User::class)->states('john-doe')->create(); //use john-doe factory state
+        $doe = factory(App\User::class)->states('john-doe')->create(); //use john-doe factory state
+        $else = factory(App\User::class, 20)->create(); //20 users will be created
 
-        factory(App\User::class, 20)->create(); //20 users will be created
+        $users = $else->concat([$doe]);  
+        
+        $posts = factory(App\BlogPost::class, 50)->make()->each(function($post) use ($users) { //created 50 blogpost
+            $post->user_id = $users->random()->id; //random users were asign to blog posts
+            $post->save(); //save is needed because we use make() function
+        });
+
+        $comments = factory(App\Comment::class, 150)->make()->each(function($comment) use ($posts) {
+            $comment->blog_post_id = $posts->random()->id;
+            $comment->save();
+        });        
     }
 }
